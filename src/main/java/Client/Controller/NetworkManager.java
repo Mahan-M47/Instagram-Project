722fd517f211a6e2e3c5e450ec3636108e3d0348
Mahan Madani <mahani.harry@gmail.com>
2021-06-04 20:35:23 +0430

    removed test code

--- src/main/java/Client/Controller/NetworkManager.java
+++ src/main/java/Client/Controller/NetworkManager.java
@@ -25,26 +25,6 @@ public class NetworkManager
 
         getThread.start();
         processThread.start();
-
-        //for testing purposes:
-//        Request req = new Request("login", new Data.Empty());
-//        while (true) {
-//            CJH.sendToServer(req);
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
-//        Request req2 = new Request("login");
-//        CJH.sendToServer(req2);
-//        CJH.sendToServer(req2);
-//        CJH.sendToServer(req2);
-//
-//        Request req = new Request.Termination();
-//        CJH.sendToServer(req);
-
     }
 
     public void stopClient() {
